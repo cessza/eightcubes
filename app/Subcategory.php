4c4e01bb8cdc0f7ad1175fadd471e3e92bdf4aa7
cessza <76838960+cessza@users.noreply.github.com>
2021-01-01 22:13:17 +0800

    Update Subcategory.php

--- app/Subcategory.php
+++ app/Subcategory.php
@@ -13,14 +13,19 @@ class Subcategory extends Model
     // public function products(){
     //     return $this->hasMany(Product::class,'subcategory_id','id','sub_name');
     // }
+    
     public function category(){
         return $this->belongsTo('App\Category','category_id')->select('id','name');
     }
     public static function subcategoryDetails($url){
-        $subcategoryDetails = Subcategory::select('id','sub_name','url','status')->where('url',$url)->first()->toArray();
-        dd($subcategoryDetails); die;
-
+        $subcategoryDetails = Subcategory::select('id','sub_name','url')->where('url',$url)->first()->toArray();
+        //dd($subcategoryDetails); die;
+        $subcatIds = array();
+        $subcatIds[] = $subcategoryDetails['id'];
+        // foreach($subcategoryDetails['products'] as $key => $product){
+        //     $subcatIds[] = $product['id'];
+        // }
         
-    return array('subcategoryDetails' => $subcategoryDetails);
+    return array('subcatIds'=>$subcatIds,'subcategoryDetails' => $subcategoryDetails);
     }
 }
