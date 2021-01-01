9a7d9f718681e70d08493a23f5931da868f2b0f9
cessza <76838960+cessza@users.noreply.github.com>
2021-01-01 22:20:50 +0800

    Update ProductsTableSeeder.php

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,8 +14,8 @@ class ProductsTableSeeder extends Seeder
     {
         //
         $productRecords = [
-            ['id'=>1, 'subcategory_id'=>1, 'category_id'=>1, 'product_name'=>'Alien Picnic Basket Black', 'product_code'=>'678BLK', 'product_price'=>80, 'product_bundle'=>480, 'product_color'=>'black', 'product_weight'=>150, 'main_image'=>'', 'product_packing'=>6, 'description'=>'', 'is_featured'=>'No', 'status'=>1],
-            ['id'=>2, 'subcategory_id'=>1, 'category_id'=>1, 'product_name'=>'Alien Picnic Basket Silver', 'product_code'=>'678S', 'product_price'=>80, 'product_bundle'=>480, 'product_color'=>'silver', 'product_weight'=>150, 'main_image'=>'', 'product_packing'=>6, 'description'=>'', 'is_featured'=>'No', 'status'=>1]
+            ['id'=>1, 'subcategory_id'=>1, 'category_id'=>1, 'product_name'=>'Alien Picnic Basket Black', 'product_code'=>'678BLK', 'product_price'=>80, 'product_bundle'=>480, 'product_color'=>'black', 'product_weight'=>150, 'url'=>'baskets', 'main_image'=>'', 'product_packing'=>6, 'description'=>'', 'is_featured'=>'No', 'status'=>1],
+            ['id'=>2, 'subcategory_id'=>1, 'category_id'=>1, 'product_name'=>'Alien Picnic Basket Colored', 'product_code'=>'678BC', 'product_price'=>80, 'product_bundle'=>480, 'product_color'=>'blue', 'product_weight'=>150, 'url'=>'baskets',  'main_image'=>'', 'product_packing'=>6, 'description'=>'', 'is_featured'=>'No', 'status'=>1]
         ];
         Product::insert($productRecords);
     }
